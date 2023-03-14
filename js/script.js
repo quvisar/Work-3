b1ae6d87a9f792e4eb97cf2ea35a05a3400eb119
quvisar <112892761+quvisar@users.noreply.github.com>
2023-03-14 23:58:17 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,5 +2,9 @@ $(document).ready(function(){
 	$('.burger').click(function(){
 		$('.navigation').toggleClass('active');
 		$('body').toggleClass('active');
-	})
+	});
+
+	$('.fourth-block-item--title').click(function(){
+		$(this).toggleClass('active');
+	});
 });
